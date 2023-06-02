84c5c8f5fa2667833189be4077687aa6103babfe
santi cuda <santiagoacuda@gmail.com>
2023-06-02 16:31:57 -0300

    added required attribute to required inputs added support for validating if both passwords are the same added span display to show if password are incorrect

--- script.js
+++ script.js
@@ -4,45 +4,112 @@ const emailInput = document.getElementById('email');
 const phoneInput = document.getElementById('phone');
 const passwordInput = document.getElementById('password');
 const confirmPasswordInput = document.getElementById('confirm-password');
-
+const submitButton = document.getElementById('submit-button');
+const passwordError = document.getElementById('password-error');
 
 firstNameInput.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
-      event.preventDefault(); // Evita el comportamiento predeterminado (recargar la página)
-      lastNameInput.focus(); // Enfoca el siguiente campo de entrada
+        event.preventDefault();
+        lastNameInput.focus();
     }
-  });
+});
 
-  lastNameInput.addEventListener('keydown', function(event) {
+lastNameInput.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
-      event.preventDefault();
-      emailInput.focus();
+        event.preventDefault();
+        emailInput.focus();
     }
-  });
+});
 
-  emailInput.addEventListener('keydown', function(event) {
+emailInput.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
-      event.preventDefault();
-      phoneInput.focus();
+        event.preventDefault();
+        phoneInput.focus();
+    }
+});
+
+phoneInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        passwordInput.focus();
+    }
+});
+
+passwordInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        confirmPasswordInput.focus();
+    }
+});
+
+confirmPasswordInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        validateForm();
+    }
+});
+
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    validateForm();
+});
+
+function validateForm() {
+    // Check for empty required inputs and display default error messages
+    const inputs = [firstNameInput, lastNameInput, emailInput, phoneInput, passwordInput, confirmPasswordInput];
+    let isValid = true;
+
+    for (let i = 0; i < inputs.length; i++) {
+        const input = inputs[i];
+        if (input.checkValidity() === false) {
+            input.reportValidity();
+            isValid = false;
+            break; // Stop the loop on the first invalid input
+        }
     }
 
-    phoneInput.addEventListener('keydown', function(event) {
-        if (event.key === 'Enter') {
-          event.preventDefault();
-          passwordInput.focus();
-        }
-      });
-    
-      passwordInput.addEventListener('keydown', function(event) {
-        if (event.key === 'Enter') {
-          event.preventDefault();
-          confirmPasswordInput.focus();
-        }
-      });
-    
+    if (!isValid) {
+        return; // Stop form submission
+    }
 
-  });
+    const password = passwordInput.value.trim();
+    const confirmPassword = confirmPasswordInput.value.trim();
 
+    // Perform password validation
+    if (password !== confirmPassword) {
+        passwordError.textContent = "Passwords do not match";
+        passwordError.classList.add('error-animation');
+        setTimeout(function() {
+            passwordError.classList.remove('error-animation');
+        }, 1000);
+        return; // Stop form submission
+    }
+
+    // If all inputs are valid, proceed with form submission
+    submitForm();
+}
+
+function submitForm() {
+    // Perform form submission logic here
+    // You can send the form data to a server or perform any other action
+    // For demonstration purposes, we'll log the form data to the console
+    console.log("Form submitted successfully!");
+    console.log("First Name:", firstNameInput.value);
+    console.log("Last Name:", lastNameInput.value);
+    console.log("Email:", emailInput.value);
+    console.log("Phone:", phoneInput.value);
+    console.log("Password:", passwordInput.value);
+    console.log("Confirm Password:", confirmPasswordInput.value);
+
+    // Reset the form (optional)
+    firstNameInput.value = "";
+    lastNameInput.value = "";
+    emailInput.value = "";
+    phoneInput.value = "";
+    passwordInput.value = "";
+    confirmPasswordInput.value = "";
+    passwordError.textContent = "";
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     var i = 0;
@@ -54,12 +121,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function typeWriter() {
         if (i <= txt.length) {
-            document.getElementById("typeWritter").innerHTML = txt.slice(0 , i + 1);
+            document.getElementById("typeWritter").innerHTML = txt.slice(0, i + 1);
             i++;
             setTimeout(typeWriter, speed);
         }
     }
 
-    
     typeWriter();
 });
